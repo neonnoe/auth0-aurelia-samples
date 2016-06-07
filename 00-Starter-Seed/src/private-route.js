7e950f093598722c439fe80289a8998f50c75e21
mehreencs87 <mehreen.cs87@gmail.com>
2016-06-07 14:57:21 +0500

    seed

---
+++ 00-Starter-Seed/src/private-route.js
@@ -0,0 +1,15 @@
+import {Redirect} from 'aurelia-router';
+import {tokenIsExpired} from './utils/tokenUtils';
+
+export class Private {
+  message = "Hello from a private route.";
+  
+  canActivate() {
+    if(tokenIsExpired()) {
+      return new Redirect('public');
+    }
+    else {
+      return true;
+    }
+  }
+}
